e7afca2b3bcb10c168e151132d115520d37f329a
Diogo Paza <diogopazacvel@gmail.com>
2023-05-16 21:15:17 -0300

    lazy loading

--- agendalive/src/app/app-routing.module.ts
+++ agendalive/src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './views/home/home.component';
 import { TableLivesComponent } from './views/table-lives/table-lives.component';
 
 const routes: Routes = [
-  {
+ /* {
     path: '',
     component: HomeComponent
-  },
+  },*/
   {
     path: 'table',
     component: TableLivesComponent
+  }, 
+  { path: 'lives', loadChildren: () => import('./views/lives/lives.module').then(m => m.LivesModule) },
+  { path: 'users', loadChildren: () => import('./views/users/users.module').then(m => m.UsersModule) },
+  {
+    path: '',
+    redirectTo: '/lives',
+    pathMatch: 'full'
   }
 ];
 
--- agendalive/src/app/app.module.ts
+++ agendalive/src/app/app.module.ts
@@ -4,62 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './views/home/home.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import { LiveListComponent } from './views/home/live-list/live-list.component';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatCardModule} from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
 import { TableLivesComponent } from './views/table-lives/table-lives.component';
-import {MatTableModule} from '@angular/material/table';
-import { LocalDateTimePipe } from './shared/pipe/local-date-time.pipe';
-import {MatChipsModule} from '@angular/material/chips';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatDialogModule} from '@angular/material/dialog';
-import { LiveFormDialogComponent } from './views/home/live-form-dialog/live-form-dialog.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-
-
-
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    LiveListComponent,
+    AppComponent,    
     TableLivesComponent,
-    LocalDateTimePipe,
-    LiveFormDialogComponent
+      
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatTabsModule,
-    MatCardModule,
+    BrowserAnimationsModule,    
     MatTableModule,
-    MatChipsModule,
-    FlexLayoutModule,
-    MatDialogModule,
-    MatInputModule,
-    MatNativeDateModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatDatepickerModule,
-    MatProgressBarModule
 
   ],
-  providers: [LocalDateTimePipe],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
